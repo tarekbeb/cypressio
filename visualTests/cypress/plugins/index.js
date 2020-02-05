40496ab11fb58f4fc5eb02ad84d128733c9ae3db
Tarek Al Beb <tariqbeb@gmail.com>
2020-02-05 14:32:07 -0600

    set up app and pkg json

---
+++ visualTests/cypress/plugins/index.js
@@ -0,0 +1,5 @@
+const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot')
+
+module.exports = (on, config) => {
+  addMatchImageSnapshotPlugin(on, config)
+}
---
+++ visualTests/cypress/support/commands.js
@@ -0,0 +1,16 @@
+import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot';
+
+addMatchImageSnapshotCommand({
+    failureTreshold: 0.00,
+    failureTresholdType: 'percent',
+    customDiffConfig: { treshold: 0.0 },
+    capture: 'viewport'
+})
+
+Cypress.Commands.add("setResolution", (size)=>{
+    if(Cypress._.isArray(size)){
+        cy.viewport(size[0], size[1])
+    } else {
+        cy.viewport(size)
+    }
+})
